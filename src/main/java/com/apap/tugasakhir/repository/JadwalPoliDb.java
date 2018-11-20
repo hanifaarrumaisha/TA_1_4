c353638f7d076760d574a5340ee1930e62051b41
nuriilmi <nurhikmahilmi94@gmail.com>
2018-11-20 14:03:52 +0700

    repository

---
+++ src/main/java/com/apap/tugasakhir/repository/JadwalPoliDb.java
@@ -0,0 +1,10 @@
+package com.apap.tugasakhir.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apap.tugasakhir.model.JadwalPoliModel;
+
+@Repository
+public interface JadwalPoliDb extends JpaRepository<JadwalPoliModel, Integer> {
+
+}
